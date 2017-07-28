b73414ada5cb016b21c8ef0813ec083fca495ddf
Carl Markham <doddsey65@gmail.com>
2017-07-28 16:30:36 +0100

    fix: Fixing missing proptypes on search page and implementing search loading state

--- src/pages/search.js
+++ src/pages/search.js
@@ -1,23 +1,52 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import Book from '../book'
+import PropTypes from 'prop-types'
+import Book from '../partials/book'
 import * as BooksAPI from '../utils/BooksAPI'
 
 export default class extends React.Component {
+  static propTypes = {
+    myBooks: PropTypes.array.isRequired,
+    onMoveBook: PropTypes.func.isRequired,
+    onRateBook: PropTypes.func.isRequired,
+  }
+
   state = {
     books: [],
+    searching: false,
   }
 
   search = (query) => {
+    if (this.state.searching) {
+      return
+    }
+    this.setState({ searching: true })
+
     if ( ! query) {
-      this.setState({ books: [] })
+      this.setState({ books: [], searching: false })
       return
     }
 
-    BooksAPI.search(query, 10).then(books => {
-      this.setState({ books })
-    }).catch(error => {
+    BooksAPI.search(query, 10).then(results => {
+      if (results.hasOwnProperty('error')) {
+        this.setState({ searching: false, books: [] })
+        return
+      }
 
+      this.props.myBooks.forEach(book => {
+        results.forEach((result, index) => {
+          if (result.id === book.id) {
+            results[index] = book
+          }
+        })
+      })
+
+      this.setState({
+        books: results,
+        searching: false
+      })
+    }).catch(error => {
+      this.setState({ searching: false })
     })
   }
 
@@ -35,15 +64,22 @@ export default class extends React.Component {
           </div>
         </div>
         <div className="search-books-results">
-          <ol className="books-grid">
-            {
-              this.state.books.map(book => (
-                <li key={book.id}>
-                  <Book book={book} onMoveBook={this.props.onMoveBook} />
-                </li>
-              ))
-            }
-          </ol>
+          {
+            this.state.searching ?
+              <h4 className="loading">Searching...</h4> :
+              <ol className="books-grid">
+                {
+                  this.state.books.map(book => (
+                    <li key={book.id}>
+                      <Book
+                        book={book}
+                        onRateBook={this.props.onRateBook}
+                        onMoveBook={this.props.onMoveBook} />
+                    </li>
+                  ))
+                }
+              </ol>
+          }
         </div>
       </div>
     )
